a63d1d9dec1081f4587b6b39bae96468c10142ca
BoomSnail <535020670@qq.com>
2017-04-13 15:12:32 +0800

    全部舞者页面

---
+++ app/src/main/java/com/knigego/nimo/demoregister/ui/activities/AllDancerActivity.java
@@ -0,0 +1,15 @@
+package com.knigego.nimo.demoregister.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.knigego.nimo.demoregister.R;
+
+public class AllDancerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_dancer);
+    }
+}
